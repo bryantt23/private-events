98a99592a1cbb9fa2fb5efd8b20c590c25b3d6f4
bryantt23 <bryantt23@gmail.com>
2015-03-15 18:03:43 -0700

    foreign keys?

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,9 @@
 class Event < ActiveRecord::Base
-  belongs_to :user
+  # belongs_to :user
+
+      belongs_to :creator, :class_name => "User"
+
+
+      # belongs_to :author, :class_name => "User"
+      # belongs_to :editor, :class_name => "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-        has_many :events
+        # has_many :events
+      has_many :events, :foreign_key => "creator_id", :class_name => "Event"
 
+
+      # has_many :authored_posts, :foreign_key => "author_id", :class_name => "Post"
+      # has_many :edited_posts, :foreign_key => "editor_id", :class_name => "Post"
 end
